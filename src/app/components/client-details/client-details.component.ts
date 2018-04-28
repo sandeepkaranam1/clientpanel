b9888db0a1df89b7ce570db4669873d91126f985
sandeepkaranam1 <sandeepkaranam@rocketmail.com>
2018-04-28 15:38:42 -0400

    editing client

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {ClientService} from '../../services/client.service';
-
-import {Router,ActivatedRoute,Params} from '@angular/router';
-import {FlashMessagesService} from 'angular2-flash-messages';
-import {Client} from '../../models/Client';
+import { ClientService } from '../../services/client.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Client } from '../../models/Client';
+import { FlashMessagesModule } from 'angular2-flash-messages/module/module';
+import { timeout } from 'q';
 
 @Component({
   selector: 'app-client-details',
@@ -11,25 +12,43 @@ import {Client} from '../../models/Client';
   styleUrls: ['./client-details.component.css']
 })
 export class ClientDetailsComponent implements OnInit {
-
-  id:string;
-  client:Client;
-  hasBalance:boolean= false;
-  showBalanceUpdateInput: boolean=false;
+  id: string;
+  client: Client;
+  hasBalance: boolean = false;
+  showBalanceUpdateInput: boolean = false;
 
   constructor(
-    private clientService:ClientService,
-    private router:Router,
-    private route:ActivatedRoute,
-    private flashMessage:FlashMessagesService
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flashMessage: FlashMessagesService
   ) { }
 
   ngOnInit() {
-    //Get Id from Url
-    this.id=this.route.snapshot.params['id'];
-    //Get Client
-    this.clientService.getClient(this.id).subscribe(client =>
-    this.client=client)
+    // Get id from url
+    this.id = this.route.snapshot.params['id'];
+    // Get client
+    this.clientService.getClient(this.id).subscribe(client => {
+      if(client != null) {
+        if(client.balance > 0) {
+          this.hasBalance = true;
+        }
+      }
+      
+      this.client = client;
+    });
   }
 
+  updateBalance(){
+    this.clientService.updateClient(this.client);
+    this.flashMessage.show('Balance Updated', {cssClass:'alert-success',timeout:4000});
+  }
+
+  onDeleteClick(){
+    if(confirm('Are you sure?')){
+      this.clientService.deleteClient(this.client);
+      this.flashMessage.show('Client Removed',{cssClass:'alert-success', timeout:4000});
+      this.router.navigate(['/']);
+    }
+  }
 }
--- src/app/components/edit-client/edit-client.component.ts
+++ src/app/components/edit-client/edit-client.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Client } from '../../models/Client';
+import { FlashMessagesModule } from 'angular2-flash-messages/module/module';
+import { timeout } from 'q';
 
 @Component({
   selector: 'app-edit-client',
@@ -7,9 +13,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditClientComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  id:string;
+  client:Client={
+    firstName:'',
+    lastName:'',
+    email:'',
+    phone:'',
+    balance:0
   }
+
+  disableBalanceOnEdit:boolean=true;
+
+  constructor(private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flashMessage: FlashMessagesService) { }
+
+    ngOnInit() {
+      // Get id from url
+      this.id = this.route.snapshot.params['id'];
+      // Get client
+      this.clientService.getClient(this.id).subscribe(client => {     
+        this.client = client;
+      });
+    }
+
+    onSubmit({value,valid}:{value:Client, valid:boolean}){
+      if(!valid){
+        this.flashMessage.show('Please Fill out the form correctly', {
+          cssClass:'alert-danger',timeout:4000
+        });
+      }else{
+        //Add id to client
+        value.id=this.id;
+        //update client
+        this.clientService.updateClient(value);
+        this.flashMessage.show('Client Updated', {
+          cssClass:'alert-success',timeout:4000
+      });
+      this.router.navigate(['/client/'+this.id]);
+    }}
 
 }
